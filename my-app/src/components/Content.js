1508189d336dd8edc0b01f45e49f56f754f164bc
Anna Gabain <anna.gabain@outlook.com>
2023-03-20 19:10:46 +0000

    Search Feature Challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import css from "./css/Content.module.css";
-import {savedPosts} from "../posts.json";
+import { savedPosts } from "../posts.json";
 import PostItem from "./PostItem";
 import Loader from "./Loader";
 
@@ -8,31 +8,53 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: [],
         }
     }
 
     componentDidMount() {
-        setTimeout(()=>{
+        setTimeout(() => {
             this.setState({
                 isLoaded: true,
+                posts: savedPosts
             })
         }, 2000)
     }
-    
+
+    dealWithChange (theEvent) {
+        let name = theEvent.target.value
+        name.toLowerCase()
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+
+        this.setState({
+            posts: filteredPosts
+        })
+    }
+
     render() {
         return (
             <div className={css.Content}>
-                
+
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput">Search:</label>
+                        <input type="search" id="seachInput" onChange={ (theEvent) => this.dealWithChange(theEvent)} />
+                        <h4>posts found: {this.state.posts.length}</h4>
+                    </form>
+
                 </div>
 
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ?
-                        <PostItem savedPosts={savedPosts} />
-                        : <Loader />
+                            // <PostItem savedPosts={savedPosts} />
+                            <PostItem savedPosts={this.state.posts} />
+
+                            : <Loader />
                     }
                 </div>
             </div>
